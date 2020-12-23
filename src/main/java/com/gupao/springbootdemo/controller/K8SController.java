00eb5ee18a4053c2229e9340c47d0c94aa314156
zougl2020 <15999966372@163.com>
2020-12-23 22:59:23 +0800

    Default Changelist

---
+++ src/main/java/com/gupao/springbootdemo/controller/K8SController.java
@@ -0,0 +1,16 @@
+package com.gupao.springbootdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+
+@RestController
+public class K8SController {
+
+
+    @RequestMapping("/k8s")
+    public String k8s(){
+        return "hello K8s <br/>111222 333";
+    }
+}
