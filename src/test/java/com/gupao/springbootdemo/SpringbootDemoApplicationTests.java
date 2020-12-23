08687f8bb6510601c8444036f084532831e19a41
zougl2020 <15999966372@163.com>
2020-12-23 23:39:36 +0800

    Default Changelist444

---
+++ src/test/java/com/gupao/springbootdemo/SpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gupao.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
