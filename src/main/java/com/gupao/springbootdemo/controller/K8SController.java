97081dba8f7d91352bb40fcd74ff4f489203645b
zougl2020 <15999966372@163.com>
2020-12-27 20:54:39 +0800

    zou

--- src/main/java/com/gupao/springbootdemo/controller/K8SController.java
+++ src/main/java/com/gupao/springbootdemo/controller/K8SController.java
@@ -11,6 +11,6 @@ public class K8SController {
 
     @RequestMapping("/k8s")
     public String k8s(){
-        return "hello K8s <br/>111222 333";
+        return "hello K8s <br/>111222";
     }
 }
